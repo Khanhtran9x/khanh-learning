1ac7d85925278bab37964e742fb019cae05194c5
khanhtran9x <khanh150795@gmail.com>
2022-10-31 08:23:42 +0700

    update

--- react-js/stock-app/src/components/Header.js
+++ react-js/stock-app/src/components/Header.js
@@ -4,23 +4,23 @@ import logo from '../images/logo.png';
 const Header = () => {
     return (
         <>
-        <div className="container-fluid header-container">
-           <div className="container">
-              <div className="row">
-              <div className="col-2">
-              <img className="navbar-logo" width="70%" src={logo} />
-              </div>
-              <div className="col-10 navbar-container">
-              <p className="navbar-item">Tin tức</p>
-              <p className="navbar-item">Thị trường</p>
-              <p className="navbar-item">Thông tin</p>
-              <p className="navbar-item">Đầu tư</p>
-              <p className="navbar-item">Đăng kí</p>
-              <button className="navbar-item-button primary-button">Đăng nhập</button>
-              </div>
-              </div>
-           </div>
-        </div>
+            <div className="container-fluid header-container">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-2">
+                            <img className="navbar-logo" width="70%" src={logo} />
+                        </div>
+                        <div className="col-10 navbar-container">
+                            <p className="navbar-item">Tin tức</p>
+                            <p className="navbar-item">Thị trường</p>
+                            <p className="navbar-item">Thông tin</p>
+                            <p className="navbar-item">Đầu tư</p>
+                            <p className="navbar-item">Đăng kí</p>
+                            <button className="navbar-item-button primary-button">Đăng nhập</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
